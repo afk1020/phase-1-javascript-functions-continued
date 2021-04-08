d29ed0a76de8729c36934389d64f96b0c1ac38ac
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-04-08 08:49:36 -0500

    completed message

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(location = "go to the office"){
+    return `This Monday, I will ${location}.`;
+};
+
+function wrapAdjective(symbol = "*"){
+    return function innerFunction(person ='a hard worker'){
+    return `You are ${symbol}${person}${symbol}!`;
+}
+};
+wrapAdjective("||")("dedicated programmer");
